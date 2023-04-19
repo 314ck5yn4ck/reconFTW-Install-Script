f03084ec5a578d379bc57acc31453a31bc5ccbf6
Steve Tenebrae <126834222+314ck5yn4ck@users.noreply.github.com>
2023-04-19 06:27:31 -0500

    Update install_reconFTW.sh

--- install_reconFTW.sh
+++ install_reconFTW.sh
@@ -11,7 +11,7 @@ ufw allow ssh
 ufw logging off # optional
 ufw enable
 
-# Disable ICMP echo requests
+# Disable ICMP echo requests on server
 echo "net.ipv4.icmp_echo_ignore_all=1" >> /etc/sysctl.conf
 sysctl -p
 
@@ -26,7 +26,7 @@ rm ~/.config/notify/provider-config.yaml
 touch ~/.config/notify/provider-config.yaml
 
 # Build Slack notifications
-# Ref - https://github.com/projectdiscovery/notify
+# Ref: https://github.com/projectdiscovery/notify
 echo "slack:" >> ~/.config/notify/provider-config.yaml
 echo "  - id: \"slack\"" >> ~/.config/notify/provider-config.yaml
 echo "    slack_channel: \"recon\"" >> ~/.config/notify/provider-config.yaml
